06e7782ad52949cc57e2d910e57895c70d725326
Kaleb Kougl <kalebkougl@gmail.com>
2018-11-26 15:39:33 -0600

    adding url parameters, adding heroku redirect and assigning ajax data to variable

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 // description = "put a language here"
-const GITHUB_JOB_URL = "https://jobs.github.com/positions.json?description="
+const HEROKU_REDIRECT = "https://cors-anywhere.herokuapp.com/"
+const GITHUB_JOB_URL = "https://jobs.github.com/positions.json?search="
+
+$(document).ready(function() {
+
+  function ajaxGetRequest(urlToCall, queryParameter){
+    $.ajax({
+      type: 'GET',
+      url: (`${HEROKU_REDIRECT}${urlToCall}${queryParameter}`),
+    }).then(function(data) {
+      console.log(data);
+      return data;
+    });
+  }
+  let githubJobData = ajaxGetRequest(GITHUB_JOB_URL, 'javascript');
+  
+});
